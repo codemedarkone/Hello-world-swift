d42ad45744b98a37e61145c9453abd14a83bca83
Chris <codemedarkone@gmail.com>
2017-08-19 19:01:06 -0600

    Finished with the alert Hello World Project

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func showMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message:"Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
